bf224b4da114b4428387ae847c5cab97b5405a58
miguel <miguel@2DAM-014>
2025-10-22 10:22:28 +0200

    principio dev_feature3

---
+++ ProjectInterfaces/src/projectinterfaces/ProjectInterfacesApplication.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectinterfaces;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import projectinterfaces.ui.ChangePasswordController;
+
+/**
+ *
+ * @author miguel
+ */
+public class ProjectInterfacesApplication extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("ui/ChangePassword.fxml"));
+        Parent root = (Parent)loader.load();
+        
+        ChangePasswordController controller = loader.getController();
+        
+        controller.init(stage);
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
