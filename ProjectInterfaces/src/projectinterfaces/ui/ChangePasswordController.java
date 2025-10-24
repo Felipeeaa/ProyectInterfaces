9ac8c09e7839581c5ae791a6ccd17cf904bb3c37
miguel <miguel@2DAM-014>
2025-10-24 14:03:43 +0200

    Avance codigo dev_feature3

--- ProjectInterfaces/src/projectinterfaces/ui/ChangePasswordController.java
+++ ProjectInterfaces/src/projectinterfaces/ui/ChangePasswordController.java
@@ -12,6 +12,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.stage.Stage;
 
@@ -30,6 +31,9 @@ public class ChangePasswordController {
     private PasswordField newPass;
     @FXML
     private PasswordField confirmNewPass;
+    @FXML
+    private Label errorLabel;
+    
     private static final Logger LOGGER = Logger.getLogger("ProjectInterfacesApplication.ui");
 
     public void init(Stage stage, Parent root) {
@@ -38,9 +42,9 @@ public class ChangePasswordController {
         //La ventana no debe ser redimensionable
         stage.setResizable(false);
         //El botón Change Password está deshabilitado hasta que los campos estén completos.
-        changePass.setDisable(true);
+        /*changePass.setDisable(true);
         //El botón Cancel está habilitado.
-        exit.setDisable(false);
+        exit.setDisable(false);*/
         //asociar eventos a manejadores
         changePass.setOnAction(this::handleChangePassOnAction);
         exit.setOnAction(this::handleExitOnAction);
@@ -59,18 +63,12 @@ public class ChangePasswordController {
      * @param oldValue
      * @param newValue 
      */
-    private void handleOldPasstextChange(ObservableValue observable,String oldValue,String newValue){
-        
-          
-    }
-    /**
-     * 
-     * @param observable
-     * @param oldValue
-     * @param newValue 
-     */
     private void handleOldPassFocusChange(ObservableValue observable,Boolean oldValue,Boolean newValue){
-        if(oldValue){
+        try{
+            if(oldValue){
+            String text = oldPass.getText();
+        }
+        }catch (Exception e){
             
         }
     }
@@ -82,7 +80,7 @@ public class ChangePasswordController {
      * @param newValue 
      */
     private void handleNewPasstextChange(ObservableValue observable,String oldValue,String newValue){
-        
+       
     }
     /**
      * 
@@ -91,8 +89,20 @@ public class ChangePasswordController {
      * @param newValue 
      */
     private void handleNewPassFocusChange(ObservableValue observable,Boolean oldValue,Boolean newValue){
-        if(oldValue){
-          
+        try{
+            if(oldValue){
+                String text = oldPass.getText();
+                boolean valid = text.matches("!-~");
+                if(text.isEmpty())
+                    throw new Exception ("Old Password is empty");
+                if(text.length()<5)
+                    throw new Exception ("The password is too short");
+                if(!valid)
+                    throw new Exception ("The password format is not valid");
+        }
+        }catch (Exception e){
+            oldPass.setStyle("-fx-border-color: red -fx-border-width: 2px;");
+            errorLabel.setText(e.getMessage());
         }
     }
     //Conform New Password
@@ -102,8 +112,18 @@ public class ChangePasswordController {
      * @param oldValue
      * @param newValue 
      */
-    private void handleConfirmNewPasstextChange(ObservableValue observable,String oldValue,String newValue){
-        
+    private void handleConfirmNewPasstextChange(ObservableValue observable,Boolean oldValue,Boolean newValue){
+       try{
+            if(oldValue){
+                String pass = oldPass.getText();
+                if(pass.length()<5){
+                    throw new Exception ("The password is not the same");  
+                }
+            }
+        }catch (Exception e){
+            oldPass.setStyle("-fx-border-color: red -fx-border-width: 2px;");
+            errorLabel.setText(e.getMessage());
+        }
     }
     /**
      * 
@@ -122,7 +142,7 @@ public class ChangePasswordController {
      * @param event 
      */
     private void handleChangePassOnAction(ActionEvent event){
-        
+       
     }
     /**
      * 
@@ -131,5 +151,16 @@ public class ChangePasswordController {
     private void handleExitOnAction(ActionEvent event){
         
     }
+    
+    private void verificarCampos() {
+        boolean oldPassValid = !oldPass.getText().trim().isEmpty();
+        boolean newPassValid = !newPass.getText().trim().isEmpty();
+        boolean confirmNewPassValid = !confirmNewPass.getText().trim().isEmpty();
+
+        // El botón solo se habilita si todos los campos tienen texto
+        boolean camposCompletos = oldPass && newPass && confirmNewPass;
+        changePass.setDisable(!camposCompletos);
+    }
+    
       
 }
