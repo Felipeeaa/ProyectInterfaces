184893de2ba4a5049d06abe7d04eac6248eb3231
Felipe <luisfelipea40@gmail.com>
2025-10-28 23:54:59 +0100

    Cambios en el funcionamiento de la ventana y en el sign in

--- ProjectInterfaces/src/projectinterfaces/ui/ProjectInterfacesController.java
+++ ProjectInterfaces/src/projectinterfaces/ui/ProjectInterfacesController.java
@@ -5,88 +5,151 @@
  */
 package projectinterfaces.ui;
 
+
 import java.util.logging.Logger;
-import javafx.beans.property.StringProperty;
-import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Hyperlink;
-import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 /**
  *
- * @author felipe
+ * @author Luis Felipe Acosta Osorno
  */
 public class ProjectInterfacesController {
     @FXML
-    private TextField Email;
+    private TextField tfEmail;
     @FXML
-    private PasswordField Password;
+    private PasswordField pfPassword;
     @FXML
-    private Hyperlink Hiperenlace;
+    private Hyperlink Register;
     @FXML
-    private Button Boton;
+    private Button btnLogin;
+    @FXML
+    private Button btnExit;
     private static final Logger LOGGER=Logger.getLogger("projectinterfaces.ui");
 
     public void init(Stage stage, Parent root) {
+        
+        //Se crea la escena asociada al grafico de root.
         LOGGER.info("Initializing window");
+        //Asociamos la escena a la primera ventana.
         Scene scene = new Scene(root);
+        //Se establecen las propiedades de la vetana.
         stage.setScene(scene);
         //Establecer el titulo de la ventana
         stage.setTitle("Sign In");
-        //• La tabla de usuarios mostrará la información de todos los usuarios existentes.
+        //La ventana no es redimensionable
         stage.setResizable(false);
-        //• Los botones Crear, Modificar y Eliminar se deshabilitan.
-        Boton.setDisable(true);
-        
-        //• Los campos Login y Nombre están habilitados.
-        //• La combo Perfil se carga con las descripciones de los diferentes perfiles de usuario.
-        //• La combo Departamento se carga con los nombres de los diferentes departamentos.
-        //• El botón Salir estará habilitado.
-        //• Se enfoca el campo login.
-        
+        //El botón Login esta deshabilitado y el botón Exit esta habilitado.
+        btnLogin.setDisable(true);
+        btnExit.setDisable(false);
         //Asociar eventos a manejadores
-        Boton.setOnAction(this::handleBotonOnAction);
-        //Asociación de manejadores a properties
-        Email.textProperty().addListener(this::handleEmailTextChange);
-        Password.focusedProperty().addListener(this::handlePasswordFochusChange);
+        //Register.setOnAction(this::handleRegisterOnAction);
+        btnLogin.setOnAction(this::handleLoginOnAction);
+        btnExit.setOnAction(this::handleExitOnAction);
+        tfEmail.textProperty().addListener((
+                observable, oldValue, newValue) -> handleChecks());
+        pfPassword.textProperty().addListener((
+                observable, oldValue, newValue) -> handleChecks());
+        
+        
+
+        
         //Mostrar la ventana
         
         stage.show();
     }
-    private void handleBotonOnAction(ActionEvent event){
+    /*private void handleRegisterOnAction(){
+        try{ 
+        FXMLLoader loader = new FXMLLoader(
+        getClass().getResource("SignUp.fxml"));
+        Parent root = (Parent)loader.load();
+        Scene scene = new Scene(root);
+        scene.getRoot();
         
-    }
-    /**
-     * 
-     * @param observable
-     * @param oldValue
-     * @param newValue 
-     */
-    private void handleEmailTextChange(ObservableValue observable, 
-                                       String oldValue,
-                                       String newValue){
+        throw new Exception("Error, al ir al registro");
         
-    }
-    /**
-     * 
-     * @param observable este metodo sirve para ver que hay en el campo texto
-     * @param oldValue este metodo sirve para
-     * @param newValue  este metodo sirve para
-     */
-    private void handlePasswordFochusChange(ObservableValue observable, 
-                                            Boolean oldValue, 
-                                            Boolean newValue){
-        if(!newValue){
-            
+        }catch(Exception e){
+           Alert alert = new Alert(
+                   AlertType.ERROR, e.getMessage(), ButtonType.OK);
         }
+    }*/
+    private void handleLoginOnAction(ActionEvent event){
+         try{
+             //Comprobación de los campos
+            if(this.tfEmail.getText().trim().equals("") || 
+                    this.pfPassword.getText().trim().equals("")){
+            throw new Exception("The email or password is incorrect");
+            }
+            
+        }catch (Exception e){
+         Alert alert = new Alert(AlertType.ERROR, e.getMessage(),
+                 ButtonType.OK);
+         alert.showAndWait();
+         if(tfEmail.getText().trim().equals("")){
+            tfEmail.setStyle("-fx-text-inner-color: red; -fx-border-width: 2;");
+         }
+         if(pfPassword.getText().trim().equals("")){
+         pfPassword.setStyle("-fx-text-inner-color: red; -fx-border-width: 2;");
+         }
+         }
+        }
+    
+    /**
+     * 
+     * @param event utilizamos este evento para manejar las excepciones
+     * necesarias a la hora de pulsar el boton Login
+     */
+    private void handleChecks(){
+        boolean fEmail = fEmail();
+        boolean fPassword = fPassword();
+        btnLogin.setDisable(!(fEmail && fPassword));
+            }
+
+    private boolean fEmail() {
+        boolean isfEmail = tfEmail.getText().isEmpty();
+        return !isfEmail;
     }
+
+    private boolean fPassword() {
+        boolean isfPassword = pfPassword.getText().isEmpty();
+        return !isfPassword;
+    }
+    
+    /**
+     * 
+     * @param event 
+     */
+    private void handleExitOnAction(ActionEvent event){
+        
+     Alert alert = new Alert(AlertType.CONFIRMATION, 
+             "Are you sure you want to go out?",
+              ButtonType.YES, ButtonType.NO);
+     alert.setTitle("Confirm Exit");
+     alert.showAndWait();
+     
+     if(alert.getResult() == ButtonType.YES){
+         //Lanzamos la ventana emergente para pedir confirmación de salida
+         Stage stage = (Stage) btnExit.getScene().getWindow();
+            stage.close();
+     }
+     
+    }
+    /**
+     * 
+     * @param event utilizamos este evento para controlar la salida del usuario
+     * antes de salir se le manda un mensaje para que confirme la salida
+     */
     
 }
 
