41b4753ad726be148635544cf87ab6110cc8febb
Felipe <luisfelipea40@gmail.com>
2025-10-28 23:58:41 +0100

    Cambios en la aplicación

--- ProjectInterfaces/src/projectinterfaces/ProjectInterfacesApplication.java
+++ ProjectInterfaces/src/projectinterfaces/ProjectInterfacesApplication.java
@@ -6,6 +6,7 @@
 package projectinterfaces;
 
 import javafx.application.Application;
+import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -21,17 +22,13 @@ public class ProjectInterfacesApplication extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         
-        FXMLLoader loader=new FXMLLoader(getClass().getResource("ui/ProyectoSignIn.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("ui/ProyectoSignIn.fxml"));
         Parent root = (Parent)loader.load();
+        ProjectInterfacesController controller = loader.getController();
+        controller.init(stage, root);
         
-        ProjectInterfacesController controller=loader.getController();
         
-        controller.init(stage);
         
-        Scene scene = new Scene(root);
-        
-        stage.setScene(scene);
-        stage.show();
     }
 
     /**
