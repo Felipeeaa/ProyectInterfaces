d12224a8bb42d00ed4a2b3b4286369554b91011c
Felipe <luisfelipea40@gmail.com>
2025-10-30 10:40:15 +0100

    Modificaciones del codigo en la rama Felipe_dev1

--- ProjectInterfaces/src/projectinterfaces/logic/CustomerRESTClient.java
+++ ProjectInterfaces/src/projectinterfaces/logic/CustomerRESTClient.java
@@ -5,23 +5,27 @@
  */
 package projectinterfaces.logic;
 
+import javax.ws.rs.ClientErrorException;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.WebTarget;
+
 /**
  * Jersey REST client generated for REST resource:CustomerFacadeREST
  * [customer]<br>
  * USAGE:
  * <pre>
- *        CustomerRESTClient client = new CustomerRESTClient();
+ *        NewJerseyClient client = new NewJerseyClient();
  *        Object response = client.XXX(...);
  *        // do whatever with response
  *        client.close();
  * </pre>
  *
- * @author luisf
+ * @author felipe
  */
 public class CustomerRESTClient {
 
-    private javax.ws.rs.client.WebTarget webTarget;
-    private javax.ws.rs.client.Client client;
+    private WebTarget webTarget;
+    private Client client;
     private static final String BASE_URI = "http://localhost:8080/CRUDBankServerSide/webresources";
 
     public CustomerRESTClient() {
@@ -29,57 +33,57 @@ public class CustomerRESTClient {
         webTarget = client.target(BASE_URI).path("customer");
     }
 
-    public void edit_XML(Object requestEntity) throws javax.ws.rs.ClientErrorException {
+    public void edit_XML(Object requestEntity) throws ClientErrorException {
         webTarget.request(javax.ws.rs.core.MediaType.APPLICATION_XML).put(javax.ws.rs.client.Entity.entity(requestEntity, javax.ws.rs.core.MediaType.APPLICATION_XML));
     }
 
-    public void edit_JSON(Object requestEntity) throws javax.ws.rs.ClientErrorException {
+    public void edit_JSON(Object requestEntity) throws ClientErrorException {
         webTarget.request(javax.ws.rs.core.MediaType.APPLICATION_JSON).put(javax.ws.rs.client.Entity.entity(requestEntity, javax.ws.rs.core.MediaType.APPLICATION_JSON));
     }
 
-    public <T> T findCustomerByEmailPassword_XML(Class<T> responseType, String email, String password) throws javax.ws.rs.ClientErrorException {
+    public <T> T findCustomerByEmailPassword_XML(Class<T> responseType, String email, String password) throws ClientErrorException {
         WebTarget resource = webTarget;
         resource = resource.path(java.text.MessageFormat.format("sigin/{0}/{1}", new Object[]{email, password}));
         return resource.request(javax.ws.rs.core.MediaType.APPLICATION_XML).get(responseType);
     }
 
-    public <T> T findCustomerByEmailPassword_JSON(Class<T> responseType, String email, String password) throws javax.ws.rs.ClientErrorException {
+    public <T> T findCustomerByEmailPassword_JSON(Class<T> responseType, String email, String password) throws ClientErrorException {
         WebTarget resource = webTarget;
         resource = resource.path(java.text.MessageFormat.format("sigin/{0}/{1}", new Object[]{email, password}));
         return resource.request(javax.ws.rs.core.MediaType.APPLICATION_JSON).get(responseType);
     }
 
-    public <T> T find_XML(Class<T> responseType, String id) throws javax.ws.rs.ClientErrorException {
+    public <T> T find_XML(Class<T> responseType, String id) throws ClientErrorException {
         WebTarget resource = webTarget;
         resource = resource.path(java.text.MessageFormat.format("{0}", new Object[]{id}));
         return resource.request(javax.ws.rs.core.MediaType.APPLICATION_XML).get(responseType);
     }
 
-    public <T> T find_JSON(Class<T> responseType, String id) throws javax.ws.rs.ClientErrorException {
+    public <T> T find_JSON(Class<T> responseType, String id) throws ClientErrorException {
         WebTarget resource = webTarget;
         resource = resource.path(java.text.MessageFormat.format("{0}", new Object[]{id}));
         return resource.request(javax.ws.rs.core.MediaType.APPLICATION_JSON).get(responseType);
     }
 
-    public void create_XML(Object requestEntity) throws javax.ws.rs.ClientErrorException {
+    public void create_XML(Object requestEntity) throws ClientErrorException {
         webTarget.request(javax.ws.rs.core.MediaType.APPLICATION_XML).post(javax.ws.rs.client.Entity.entity(requestEntity, javax.ws.rs.core.MediaType.APPLICATION_XML));
     }
 
-    public void create_JSON(Object requestEntity) throws javax.ws.rs.ClientErrorException {
+    public void create_JSON(Object requestEntity) throws ClientErrorException {
         webTarget.request(javax.ws.rs.core.MediaType.APPLICATION_JSON).post(javax.ws.rs.client.Entity.entity(requestEntity, javax.ws.rs.core.MediaType.APPLICATION_JSON));
     }
 
-    public <T> T findAll_XML(Class<T> responseType) throws javax.ws.rs.ClientErrorException {
+    public <T> T findAll_XML(Class<T> responseType) throws ClientErrorException {
         WebTarget resource = webTarget;
         return resource.request(javax.ws.rs.core.MediaType.APPLICATION_XML).get(responseType);
     }
 
-    public <T> T findAll_JSON(Class<T> responseType) throws javax.ws.rs.ClientErrorException {
+    public <T> T findAll_JSON(Class<T> responseType) throws ClientErrorException {
         WebTarget resource = webTarget;
         return resource.request(javax.ws.rs.core.MediaType.APPLICATION_JSON).get(responseType);
     }
 
-    public void remove(String id) throws javax.ws.rs.ClientErrorException {
+    public void remove(String id) throws ClientErrorException {
         webTarget.path(java.text.MessageFormat.format("{0}", new Object[]{id})).request().delete();
     }
 
